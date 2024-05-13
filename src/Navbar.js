99852f1d14497fffd4ca2778bceac8bcbb6b3d94
Joseph Ogelenya <josephoges9@gmail.com>
2024-05-13 01:42:52 +0100

    Built dropdown component, and made navbar responsive.

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,17 @@
 import "./styles/Navbar.css";
 import { Link } from "react-router-dom";
 import testImg from "./images/testImg.svg";
+import logo from "./images/logo.svg";
+import Dropdown from "./components/Dropdown";
 
 const Navbar = () => {
   return (
     <nav className='navbar'>
       <div className='navbar__logo-container'>
         <img
-          src={testImg}
+          src={logo}
           alt=''
         />
-        <p>ada</p>
       </div>
       <div className='navbar__nav-links '>
         <Link
@@ -18,8 +19,35 @@ const Navbar = () => {
           className='nav-links__link'>
           Pricing
         </Link>
-        <span></span>
-        <div className='nav-links__menu-wrapper' >
+        {/* <span></span> */}
+        <Dropdown
+          title={"Platform"}
+          content={[
+            { src: "", title: "Testing" },
+            { src: "", title: "Platform" },
+            { src: "", title: "Elements" },
+          ]}
+        />
+        <Dropdown
+          title={"Solutions"}
+          content={[
+            { src: "", title: "Computed" },
+            { src: "", title: "Testing" },
+            { src: "", title: "Learn more" },
+            { src: "", title: "Learn more" },
+          ]}
+        />
+        <Dropdown
+          title={"Resources"}
+          content={[
+            { src: "", title: "Learn more" },
+            { src: "", title: "Testing" },
+            { src: "", title: "Resources" },
+            { src: "", title: "Styles" },
+            { src: "", title: "Layout" },
+          ]}
+        />
+        {/* <div className='nav-links__menu-wrapper'>
           <button
             type='button'
             className='nav-links__link flex'>
@@ -28,7 +56,7 @@ const Navbar = () => {
           </button>
           <div className='nav-menu'></div>
         </div>
-        <div className='nav-links__menu-wrapper' >
+        <div className='nav-links__menu-wrapper'>
           <button
             type='button'
             className='nav-links__link flex'>
@@ -37,7 +65,7 @@ const Navbar = () => {
           </button>
           <div className='nav-menu'></div>
         </div>
-        <div className='nav-links__menu-wrapper' >
+        <div className='nav-links__menu-wrapper'>
           <button
             type='button'
             className='nav-links__link flex'>
@@ -45,7 +73,7 @@ const Navbar = () => {
             <img src='' />
           </button>
           <div className='nav-menu'></div>
-        </div>
+        </div> */}
       </div>
       <Link
         to='/getDemo'
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,9 +1,33 @@
-// import ../styles/Dropdown.js
+import "../styles/Dropdown.css";
+import chevron from "../images/chevron.svg";
 
-const Dropdown = ({}) => {
-    return (
-        <>DROPDOWN</>
-    )
-}
+const Dropdown = ({ title, content }) => {
+  return (
+    <label className='dropdown-wrapper'>
+      <input
+        type='checkbox'
+        name='menu'
+        id=''
+      />
+      <p>{title}</p>
+      <img
+        class='arrowimg'
+        src={chevron}
+      />
+      <ul class='menu'>
+        {content.map((item) => {
+          return (
+            <li key={1}>
+              <a href={item.src}>
+                {/* <img src="images/icon-todo.svg" alt="todo icon"/> */}
+                <p>{item.title}</p>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </label>
+  );
+};
 
 export default Dropdown;
